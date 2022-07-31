ac4327b9beee42c006d9dcc06d3b78349c6c3b42
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-07-31 15:11:26 +0800

    Add files via upload

---
+++ 004/解答.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,front,back,a1,a2,a3,b,c=0;
+    for (int i = 991; i < 1000; i++)
+    {
+        for (int j = 101; j < 1000; j++)
+        {
+            num = i * j;
+
+            front = num/1000;
+            back = num % 1000;
+            
+            a1 = back % 10;
+            a2 = back % 100 / 10;
+            a3 = back /100;
+            
+            b = a1*100+a2*10+a3;
+
+            if (front == b)
+            {
+                if (num > c && num > 900000)
+                {
+                    c = num;
+                    printf("%d*%d=%d\n",i,j,num);
+                }
+            }
+            
+        }
+        
+    }
+    
+}
